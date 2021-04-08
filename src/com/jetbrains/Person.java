bf995c60f528b1405f05ce92ad8e46da78a356cd
xbreathoflife <xbreathoflife@gmail.com>
2021-04-09 00:26:00 +0300

    Complete task

---
+++ src/com/jetbrains/Person.java
@@ -0,0 +1,19 @@
+package com.jetbrains;
+
+public class Person {
+    private String name;
+    private int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return  name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
